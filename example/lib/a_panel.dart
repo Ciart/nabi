8cf2b72063509e7352371184562ecde249c67d13
Pdom <ahb0327@naver.com>
2021-07-04 22:08:09 +0900

    Create nabi widget

---
+++ example/lib/a_panel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class APanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("A"),
+      ),
+      color: const Color(0xff883333),
+    );
+  }
+}
