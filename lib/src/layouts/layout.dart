fa0e0686cf3c30a3ef9aefa84fb4021d16078f61
Pdom <ahb0327@naver.com>
2022-01-17 17:20:50 +0900

    c

--- lib/src/layouts/layout.dart
+++ lib/src/layouts/layout.dart
@@ -27,11 +27,44 @@ class Layout {
     return findItem(targetId, root);
   }
 
-  void addChild(String parentId, LayoutItem layout) {
+  LayoutGroup? findParent(String childId, LayoutItem layout) {
+    if (layout is! LayoutGroup) {
+      return null;
+    }
+
+    for (var child in layout.children) {
+      if (child.id == childId) {
+        return layout;
+      }
+
+      var result = findParent(childId, child);
+
+      if (result != null) {
+        return result;
+      }
+    }
+  }
+
+  LayoutGroup? findParentInRoot(String childId) {
+    return findParent(childId, root);
+  }
+
+  void addChild(String parentId, LayoutWidget layoutWidget) {
     var parent = findItemInRoot(parentId);
 
     if (parent is LayoutGroup) {
-      parent.addChild(layout);
+      var parentOfParent = findParentInRoot(parentId);
+
+      if (parentOfParent == null) {
+        return;
+      }
+
+      for (int i = 0; i < parentOfParent.children.length; i++) {
+        if (parentOfParent.children[i].id == parentId) {
+          parentOfParent.children[i] =
+              parent.copyWith(children: [...parent.children, layoutWidget]);
+        }
+      }
     }
   }
 
